e6dbb3d5adcd92977c1ba945f18fe72b1968dfad
Ivan <ivanezeh12@gmail.com>
2023-12-30 20:54:36 +0100

    dd

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import { AnimatePresence } from "framer-motion";
+
+import Ori from "./pages/Other";
+
+function App() {
+  const location = useLocation()
+
+  return (
+      <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Ori />}>
+          
+        </Route>
+      </Routes>
+      </AnimatePresence>
+  );
+}
+
+export default App;
