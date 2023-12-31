460a61f5b0e4108f88fdbe66a06d89dc7925512e
Ivan <ivanezeh12@gmail.com>
2023-12-31 19:34:48 +0100

    first touches

--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -1,5 +1,9 @@
 export default function Ori (){
     return(
-        <div>poopy butt</div>
+        <div classname="container">
+            <h1>I love to cook</h1>
+            <span>My name is Ivan and I love to cook meth</span>
+            <span>This cannot be used as evidence against me i a court of law though.</span>
+        </div>
     )
 }
