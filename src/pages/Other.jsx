8b0c3da4c6d2170882d61a5d5edeeb7df6216e7d
Ivan <ivanezeh12@gmail.com>
2024-01-02 23:17:55 +0100

    juu

--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -2,7 +2,7 @@ export default function Ori (){
     return(
         <div className="container">
             <h1>I love to cook</h1>
-            <span>My name is Ivan and I love to cook meth</span>
+            <span>My name is Ivan an....d I love to cook meth</span>
             <span className="subtext">This cannot be used as evidence against me i a court of law though.</span>
         </div>
     )
