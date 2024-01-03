b3bd26ff4668c43c3aa3a4babc8eb40794904c93
Ivan <ivanezeh12@gmail.com>
2024-01-03 16:06:54 +0100

    opo

--- src/components_oriflame/Card.jsx
+++ src/components_oriflame/Card.jsx
@@ -1,23 +1,48 @@
-export default function Card(props){
-
-    return(
-        <div className="Card">
-            <div className="container">
-                <img />
-                <div className="Main">
-                    <div className="left">
-                        <span></span>
-                        <span></span>
-                    </div>
-                    <div className="rite">
-                        <span></span>
-                    </div>
-                </div>
-                <div className="details">
-                    <div className="left"></div>
-                    <div className="rite"></div>
-                </div>
-            </div>
+export default function Card(props) {
+  return (
+    <div className="Card">
+      <div className="container">
+        <img />
+        <div className="Main">
+          <div className="left">
+            <span>{props.firstname}</span>
+            <span>{props.lastname}</span>
+          </div>
+          <div className="rite">
+            <span>{props.overall}</span>
+          </div>
         </div>
-    )
+        <div className="details">
+          <ul className="left">
+            <li>
+              Attack
+              <span>{props.attack}</span>
+            </li>
+            <li>
+              Defense
+              <span>{props.defense}</span>
+            </li>
+            <li>
+              Stamina
+              <span>{props.stamina}</span>
+            </li>
+          </ul>
+          <ul className="rite">
+            <li>
+              Accuracy
+              <span>{props.accuracy}</span>
+            </li>
+            <li>
+              Passing
+              <span>{props.passing}</span>
+            </li>
+            <li>
+                handles
+              <span>{props.handles}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
