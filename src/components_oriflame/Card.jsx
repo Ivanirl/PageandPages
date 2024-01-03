9b14804cfe1b76e3f0428679c5f4d9e3643d8227
Ivan <ivanezeh12@gmail.com>
2024-01-03 10:57:39 +0100

    eart

--- src/components_oriflame/Card.jsx
+++ src/components_oriflame/Card.jsx
@@ -1,7 +1,22 @@
 export default function Card(){
     return(
         <div className="Card">
-            
+            <div className="container">
+                <img />
+                <div className="Main">
+                    <div className="left">
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div className="rite">
+                        <span></span>
+                    </div>
+                </div>
+                <div className="details">
+                    <div className="left"></div>
+                    <div className="rite"></div>
+                </div>
+            </div>
         </div>
     )
 }
