1bdb928c0c536498c13d840ab40e42ed84178d14
Ivan <ivanezeh12@gmail.com>
2024-01-12 14:14:06 +0100

    hhhh

--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -1,20 +1,35 @@
 import Card from "../components_oriflame/Card";
-import kin from "./Data.json";
+import Pdata from "./Data.json";
 
 export default function Ori() {
+  const poop = Pdata.LeBron;
+  const spice = Pdata.Curry
+
   return (
     <div className="container">
       <Card
-        key={kin.id}
-        firstname={kin.firstname}
-        lastname={kin.lastname}
-        attack={kin.attack}
-        defense={kin.defense} 
-        stamina={kin.stamina}
-        accuracy={kin.accuracy  }
-        passing={kin.passing}
-        handles={kin.handles}
-        overall={kin.overall}
+        key={poop.id}
+        firstname={poop.firstname}
+        lastname={poop.lastname}
+        attack={poop.attack}
+        defense={poop.defense}
+        stamina={poop.stamina}
+        accuracy={poop.accuracy}
+        passing={poop.passing}
+        handles={poop.handles}
+        overall={poop.overall}
+      />
+      <Card
+        key={spice.id}
+        firstname={spice.firstname}
+        lastname={spice.lastname}
+        attack={spice.attack}
+        defense={spice.defense}
+        stamina={spice.stamina}
+        accuracy={spice.accuracy}
+        passing={spice.passing}
+        handles={spice.handles}
+        overall={spice.overall}
       />
     </div>
   );
