da579346f936fef44f1e48bc6cbbbba86cbd06dc
Ivan <ivanezeh12@gmail.com>
2024-01-12 17:35:24 +0100

    pop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,14 @@ export default function Home() {
   return (
     <div className="Homey">
       <section className="FirstAvenger">
-        <h1>Welcome<br></br> to your nightmare</h1>
-        <span>Hahahahaha</span>
+        <h1>
+          Welcome<br></br> to your nightmare
+        </h1>
+        <span className="top">Hahahahaha</span>
+        <span className="small">
+          I'll explain this for the civilians looking at this demo page, this is
+          a Gloria from Mordern Family reference.
+        </span>
       </section>
     </div>
   );
