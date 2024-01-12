a2cb32a4a59bf1b0ee684e3b33a61d37719d4fbb
Ivan <ivanezeh12@gmail.com>
2024-01-12 15:45:43 +0100

    uu

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ import {
 import { AnimatePresence } from "framer-motion";
 
 import Ori from "./pages/Other";
+import Home from "./pages/Home";
 
 function App() {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
-      <AnimatePresence>
+    <AnimatePresence>
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Ori />}>
-          
-        </Route>    
+        <Route path="/" element={<Ori />}></Route>
+        <Route path="/ho" element={<Home />}></Route>
       </Routes>
-      </AnimatePresence>
+    </AnimatePresence>
   );
 }
 
