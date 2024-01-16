1fb423b260d8670dee684f78e298862cf188b097
Ivan <ivanezeh12@gmail.com>
2024-01-16 14:33:08 +0100

    ret

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from "react-router-dom";
 
+import Navbar from "../components_oriflame/Navbar";
+
 export default function Home() {
   const bro = useNavigate();
 
   return (
     <div className="Homey">
+      <Navbar/>
       <section className="FirstAvenger">
         <h1>
           Welcome<br></br> to your nightmare
