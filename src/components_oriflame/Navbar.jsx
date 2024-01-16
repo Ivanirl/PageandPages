5b27c738bac4eab6abf590668e75aefe0838506d
Ivan <ivanezeh12@gmail.com>
2024-01-16 14:33:24 +0100

    tson

---
+++ src/components_oriflame/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return(
+        <div className="Navbar">
+            <div className="container">
+                <div className="left"></div>
+                <div className="right">
+                    <span>ORDER ONLINE</span>
+                </div>
+            </div>
+        </div>
+    )
+}
