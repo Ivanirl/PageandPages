1743a9e6058291b4104f5cc1d606928063ec155a
Ivan <ivanezeh12@gmail.com>
2024-01-17 21:33:17 +0100

    ppp

--- src/components_oriflame/Navbar.jsx
+++ src/components_oriflame/Navbar.jsx
@@ -1,14 +1,14 @@
 //import { useState} from "react"
 
-export default function Navbar({menuha, setMenuha}){
+export default function Navbar({toint, menuha, onyx, setMenuha}){
     //const [menuha, setMenuha] = useState(false)
 
     return(
         <div className="Navbar">
             <div className="container">
-                <button className={menuha? "left": "rite"} onClick={()=>{setMenuha(!menuha)}}></button>
+                <button className={menuha? "left": "rite"} onClick={onyx}></button>
                 <div className="right">
-                    <span className={menuha? " ": "blue"}>ORDER ONLINE</span>
+                    <span className={menuha? " ": "blue"}>{toint}</span>
                 </div>
             </div>
         </div>
