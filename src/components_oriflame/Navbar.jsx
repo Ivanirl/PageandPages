7ed51774d894baf0a548b260bc99d0d9dfd559d3
Ivan <ivanezeh12@gmail.com>
2024-01-17 22:00:25 +0100

    www

--- src/components_oriflame/Navbar.jsx
+++ src/components_oriflame/Navbar.jsx
@@ -2,9 +2,9 @@ export default function Navbar({ toint, yup, onyx }) {
   return (
     <div className="Navbar">
       <div className="container">
-        <button className={yup ? "left" : "rite"} onClick={onyx}></button>
+        <button className={yup ? "rite" : "left"} onClick={onyx}></button>
         <div className="right">
-          <span className={yup ? " " : "blue"}>{toint}</span>
+          <span className={yup ? "blue" : " "}>{toint}</span>
         </div>
       </div>
     </div>
