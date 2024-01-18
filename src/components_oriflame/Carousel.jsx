67c16d95e34d7ec06fc01901c06fc55efe48efd0
Ivan <ivanezeh12@gmail.com>
2024-01-18 10:42:03 +0100

    wery

--- src/components_oriflame/Carousel.jsx
+++ src/components_oriflame/Carousel.jsx
@@ -2,7 +2,7 @@ export default function Carousel(){
     return(
         <div className="Caro">
             <div className="box"><span>Thomas Partey</span></div>
-            {/* <div className="box">Thomas Partey</div> */}
+            <div className="box">Arsene Wenger's Left Nut</div>
             {/* <div className="box">Thomas Partey</div> */}
         </div>
     )
