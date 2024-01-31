99a713b8e83ba8b6e7c591943ff837a2aabbfc81
Ivan <ivanezeh12@gmail.com>
2024-01-31 13:54:51 +0100

    bbb

--- src/components_oriflame/Card.jsx
+++ src/components_oriflame/Card.jsx
@@ -11,7 +11,6 @@ export default function Card(props) {
             <span className="last">{props.lastname}</span>
           </div>
           <div className="rite">
-            dobe 
             <span>{props.overall}</span>
           </div>
         </div>
